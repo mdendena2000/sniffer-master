5edc2761b3ee7512fd7ab42af8e3cd8217f26e27
root <matheus-dendena@outlook.com>
2022-04-12 13:18:17 -0300

    Updated Layout Print

--- sniffer.py
+++ sniffer.py
@@ -11,13 +11,11 @@ from components.http import HTTP
 
 TAB_1 = '\t - '
 TAB_2 = '\t\t - '
-TAB_3 = '\t\t\t - '
-TAB_4 = '\t\t\t\t - '
 
 DATA_TAB_1 = '\t   '
-DATA_TAB_2 = '\t\t   '
-DATA_TAB_3 = '\t\t\t   '
-DATA_TAB_4 = '\t\t\t\t   '
+
+# verde = '\033[32m'
+# restaura cor original = '\033[0;0m'
 
 def main():
 
@@ -25,12 +23,12 @@ def main():
     # Cria um socket de rede utilizando funções nativas do python
     sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
         
-    # Escuta na porta 65565
+    # Escuta na porta 65565 // 65535
     data, addr = sock.recvfrom(65535)
     pcap.write(data)
     eth = Ethernet(data)
 
-    print('\nEthernet Frame:')
+    print('\033[32m' + '\nEthernet Frame:' + '\033[0;0m')
     print(TAB_1 + 'Destination: {}'.format(eth.dest_mac))
     print(TAB_1 + 'Source: {}'.format(eth.src_mac))
     print(TAB_1 + 'Protocol: {}'.format(eth.proto))   
@@ -39,7 +37,7 @@ def main():
     # IPv4
     if (eth.proto == 8):
         ipv4 = IPv4(eth.data)
-        print('\nIPV4:')
+        print('\033[32m' + '\nIPV4:' + '\033[0;0m')
         print(TAB_1 + 'Version: {}'.format(ipv4.version))
         print(TAB_1 + 'Header Length: {}'.format(ipv4.header_length))
         print(TAB_1 + 'TTL: {}'.format(ipv4.ttl))
@@ -51,7 +49,7 @@ def main():
         # ICMP
         if (ipv4.proto == 1):
             icmp = ICMP(ipv4.data)
-            print('\nICMP:')
+            print('\033[32m' + '\nICMP:' + '\033[0;0m')
             print(TAB_1 + 'Type: {}'.format(icmp.type))
             print(TAB_1 + 'Code: {}'.format(icmp.code))
             print(TAB_1 + 'Checksum: {}'.format(icmp.checksum))
@@ -62,20 +60,20 @@ def main():
         # TCP
         elif (ipv4.proto == 6):
             tcp = TCP(ipv4.data)
-            print('\nTCP:')
+            print('\033[32m' + '\nTCP:' + '\033[0;0m')
             print(TAB_1 + 'Source Port: {}'.format(tcp.src_port))
             print(TAB_1 + 'Destination Port: {}'.format(tcp.dest_port))
             print(TAB_1 + 'Sequence: {}'.format(tcp.sequence))
             print(TAB_1 + 'Acknowledgment: {}'.format(tcp.acknowledgment))
 
-            print('\n' + DATA_TAB_1 + 'Flags:')
+            print('\033[32m' + '\n' + DATA_TAB_1 + 'Flags:' + '\033[0;0m')
             print(TAB_2 + 'URG: {}, ACK: {}, PSH: {}'.format(tcp.flag_urg, tcp.flag_ack, tcp.flag_psh))
             print(TAB_2 + 'RST: {}, SYN: {}, FIN:{}'.format(tcp.flag_rst, tcp.flag_syn, tcp.flag_fin))
 
             if len(tcp.data) > 0:
                 # HTTP
                 if (tcp.src_port == 80 or tcp.dest_port) == 80:
-                    print('\nHTTP Data:')
+                    print('\033[32m' + '\nHTTP Data:' + '\033[0;0m')
                     try:
                         http = HTTP(tcp.data)
                         http_info = str(http.data).split('\n')
@@ -86,40 +84,40 @@ def main():
                         print(format_multi_line(DATA_TAB_1, tcp.data))
                
                 else:
-                    print('\nTCP Data:')
+                    print('\033[32m' + '\nTCP Data:' + '\033[0;0m')
                     print(format_multi_line(DATA_TAB_1, tcp.data))
 
             # UDP
             elif (ipv4.proto == 17):
                 udp = UDP(ipv4.data)
-                print('\nUDP:')
+                print('\033[32m' + '\nUDP:' + '\033[0;0m')
                 print(TAB_1 + 'Source Port: {}'.format(udp.src_port))
                 print(TAB_1 + 'Destination Port: {}'.format(udp.dest_port))
                 print(TAB_1 + 'Length: {}'.format(udp.size))
-
             # Outros IPv4
             else:
-                print('\nOutros IPV4 Data:')
+                print('\033[32m' + '\nOutros IPV4 Data:' + '\033[0;0m')
                 print(format_multi_line(DATA_TAB_1, ipv4.data))
 
-        # IPv6
-        elif (eth.proto == 56710):
-            ipv6 = IPv6(eth.data)
-            print('\nIPV6:')
-            print(TAB_1 + 'Version: {}'.format(ipv6.version))
-            print(TAB_1 + 'Traffic Class: {}'.format(ipv6.traffic_class))
-            print(TAB_1 + 'Flow Label: {}'.format(ipv6.flow_label))
+    # IPv6
+    elif (eth.proto == 56710):
+        ipv6 = IPv6(eth.data)
+        print('\033[32m' + '\nIPV6:' + '\033[0;0m')
+        print(TAB_1 + 'Version: {}'.format(ipv6.version))
+        print(TAB_1 + 'Traffic Class: {}'.format(ipv6.traffic_class))
+        print(TAB_1 + 'Flow Label: {}'.format(ipv6.flow_label))
 
-            print(TAB_1 + 'Payload Length: {}'.format(ipv6.payload_length))
-            print(TAB_1 + 'Next Header: {}'.format(ipv6.next_header))
-            print(TAB_1 + 'Hop Limit: {}'.format(ipv6.hop_limit))
+        print(TAB_1 + 'Payload Length: {}'.format(ipv6.payload_length))
+        print(TAB_1 + 'Next Header: {}'.format(ipv6.next_header))
+        print(TAB_1 + 'Hop Limit: {}'.format(ipv6.hop_limit))
 
-            print(TAB_1 + 'Source Address: {}'.format(ipv6.source_address))
-            print(TAB_1 + 'Destination Address: {}'.format(ipv6.destination_address))
+        print(TAB_1 + 'Source Address: {}'.format(ipv6.source_address))
+        print(TAB_1 + 'Destination Address: {}'.format(ipv6.destination_address))
                     
     else:
-        print('\nEthernet Data:')
+        print('\033[32m' + '\nEthernet Data:' + '\033[0;0m')
         print(format_multi_line(DATA_TAB_1, eth.data))        
         #fecha arquivo       
         pcap.close()   
+
 main()
